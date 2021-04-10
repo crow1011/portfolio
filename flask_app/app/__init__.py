c814c3d81d1154a4f0dc98c17f77b4cdb24d8e0a
crow1011 <crowsmith1011@gmail.com>
2021-04-10 13:19:03 +0300

    add mod_gen bp

--- flask_app/app/__init__.py
+++ flask_app/app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
-from app.controllers import mod_tst
+from app.controllers import mod_tst, mod_gen
 
 app = Flask(__name__)
 app.register_blueprint(mod_tst)
+app.register_blueprint(mod_gen)
+
--- flask_app/app/controllers.py
+++ flask_app/app/controllers.py
@@ -1,8 +1,20 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+from .utils import info
+
+
 
 mod_tst = Blueprint('tst', __name__, url_prefix='/tst')
+mod_gen = Blueprint('gen', __name__, url_prefix='/gen')
 
 
 @mod_tst.route('/', methods=['POST', 'GET'])
 def tst():
     return 'OK'
+
+
+@mod_gen.route('', methods=['POST', 'GET'])
+def gen():
+    if request.method == 'GET':
+        return jsonify(info('invalid_nethod'))
+    print(request.json)
+    return 'nice'
