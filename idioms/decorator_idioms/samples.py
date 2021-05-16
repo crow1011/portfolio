e33e048e27e6ff02fcb23602e62cc8892997a04f
crow1011 <crowsmith1011@gmail.com>
2021-05-16 20:43:56 +0300

    add trace decorator

--- idioms/decorator_idioms/samples.py
+++ idioms/decorator_idioms/samples.py
@@ -13,3 +13,21 @@ foo()
 
 # args_audit decorator
 
+def trace_decorator(func):
+    ''' print args, kwargs and func result '''
+    def wrapper(*args, **kwargs):
+        print(f'_-_- start trace {func.__name__}() _-_-')
+        print(f'args: {args}')
+        print(f'kwargs: {kwargs}')
+        print(f'RUN {func.__name__}(*args, **kwargs)')
+        original_res = func(*args, **kwargs)
+        print(f'func RETURN: {original_res}')
+        print(f'_-_- end trace {func.__name__}() _-_-')
+        return original_res
+    return wrapper
+
+@trace_decorator
+def spam(a, b):
+    return a+b
+
+spam(1,b=2)
