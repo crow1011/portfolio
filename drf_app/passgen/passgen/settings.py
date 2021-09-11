ce327aa29f4afa15ad40c91c34ebb105501b7bc8
crow1011 <crowsmith1011@gmail.com>
2021-09-11 09:29:57 +0300

    add env

--- drf_app/passgen/passgen/settings.py
+++ drf_app/passgen/passgen/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a-u0%0^g0-(9^sf$q0pbwsinf2luw5-2wc@m2@%0564sqoysj#'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "local-dev-default-u0%0^g0-(9^sf$q0pbwsinf2luw5-2wc@m2@%0564sqoysj#")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", False)
 
 ALLOWED_HOSTS = []
 
